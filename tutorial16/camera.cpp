f1caa25c10f683dd2a7970ee852202221ea78d57
Ilya <triplepointfive@gmail.com>
2012-10-14 18:34:04 +0400

    Changed camera class, now its more comfortable and looks like in FPS games

--- tutorial16/camera.cpp
+++ tutorial16/camera.cpp
@@ -1,20 +1,3 @@
-/*
-
-	Copyright 2010 Etay Meiri
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 #include <GL/freeglut.h>
 
 #include "camera.h"
@@ -55,7 +38,7 @@ void Camera::Init()
 {
     Vector3f HTarget(m_target.x, 0.0, m_target.z);
     HTarget.Normalize();
-    
+
     if (HTarget.z >= 0.0f)
     {
         if (HTarget.x >= 0.0f)
@@ -78,13 +61,9 @@ void Camera::Init()
             m_AngleH = 90.0f + ToDegree(asin(-HTarget.z));
         }
     }
-    
+
     m_AngleV = -ToDegree(asin(m_target.y));
 
-    m_OnUpperEdge = false;
-    m_OnLowerEdge = false;
-    m_OnLeftEdge  = false;
-    m_OnRightEdge = false;
     m_mousePos.x  = m_windowWidth / 2;
     m_mousePos.y  = m_windowHeight / 2;
 
@@ -142,69 +121,21 @@ void Camera::OnMouse(int x, int y)
     const int DeltaX = x - m_mousePos.x;
     const int DeltaY = y - m_mousePos.y;
 
-    m_mousePos.x = x;
-    m_mousePos.y = y;
+    if ((DeltaX == 0)&&(DeltaY == 0)) return;
 
     m_AngleH += (float)DeltaX / 20.0f;
     m_AngleV += (float)DeltaY / 20.0f;
 
-    if (DeltaX == 0) {
-        if (x <= MARGIN) {
-        //    m_AngleH -= 1.0f;
-            m_OnLeftEdge = true;
-        }
-        else if (x >= (m_windowWidth - MARGIN)) {
-        //    m_AngleH += 1.0f;
-            m_OnRightEdge = true;
-        }
-    }
-    else {
-        m_OnLeftEdge = false;
-        m_OnRightEdge = false;
-    }
 
-    if (DeltaY == 0) {
-        if (y <= MARGIN) {
-            m_OnUpperEdge = true;
-        }
-        else if (y >= (m_windowHeight - MARGIN)) {
-            m_OnLowerEdge = true;
-        }
-    }
-    else {
-        m_OnUpperEdge = false;
-        m_OnLowerEdge = false;
-    }
 
     Update();
+    glutWarpPointer(m_windowWidth / 2, m_windowHeight / 2);
 }
 
 
 void Camera::OnRender()
 {
     bool ShouldUpdate = false;
-
-    if (m_OnLeftEdge) {
-        m_AngleH -= 0.1f;
-        ShouldUpdate = true;
-    }
-    else if (m_OnRightEdge) {
-        m_AngleH += 0.1f;
-        ShouldUpdate = true;
-    }
-
-    if (m_OnUpperEdge) {
-        if (m_AngleV > -90.0f) {
-            m_AngleV -= 0.1f;
-            ShouldUpdate = true;
-        }
-    }
-    else if (m_OnLowerEdge) {
-        if (m_AngleV < 90.0f) {
-           m_AngleV += 0.1f;
-           ShouldUpdate = true;
-        }
-    }
 
     if (ShouldUpdate) {
         Update();
@@ -224,7 +155,7 @@ void Camera::Update()
     Vector3f Haxis = Vaxis.Cross(View);
     Haxis.Normalize();
     View.Rotate(m_AngleV, Haxis);
-       
+
     m_target = View;
     m_target.Normalize();
 
--- tutorial16/main.cpp
+++ tutorial16/main.cpp
@@ -1,23 +1,3 @@
-/*
-
-	Copyright 2011 Etay Meiri
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-    Tutorial 16 - Basic Texture Mapping
-*/
-
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
@@ -29,8 +9,8 @@
 #include "camera.h"
 #include "texture.h"
 
-#define WINDOW_WIDTH  1920
-#define WINDOW_HEIGHT 1200
+#define WINDOW_WIDTH  1280
+#define WINDOW_HEIGHT 1024
 
 struct Vertex
 {
@@ -155,7 +135,7 @@ static void CreateVertexBuffer()
                            Vertex(Vector3f(0.0f, -1.0f, -1.15475), Vector2f(0.5f, 0.0f)),
                            Vertex(Vector3f(1.0f, -1.0f, 0.5773f),  Vector2f(1.0f, 0.0f)),
                            Vertex(Vector3f(0.0f, 1.0f, 0.0f),      Vector2f(0.5f, 1.0f)) };
-    
+
  	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
@@ -250,7 +230,7 @@ int main(int argc, char** argv)
     glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("Tutorial 16");
-    glutGameModeString("1920x1200@32");
+    glutGameModeString("1280x1024@32");
     glutEnterGameMode();
 
     InitializeGlutCallbacks();
@@ -281,7 +261,7 @@ int main(int argc, char** argv)
     if (!pTexture->Load()) {
         return 1;
     }
-       
+
     glutMainLoop();
 
     return 0;
